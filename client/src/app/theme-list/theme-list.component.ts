a78e29f7cbdb14c361c0fd33935a360bdd34cf6f
planimir77 <planimir7708@gmail.com>
2020-11-16 18:36:32 +0200

    Add sort by created date

--- client/src/app/theme-list/theme-list.component.ts
+++ client/src/app/theme-list/theme-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ITheme } from '../theme.interface';
 import { ThemeService } from './theme.service';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-theme-list',
@@ -11,13 +12,23 @@ import { ThemeService } from './theme.service';
 
 export class ThemeListComponent implements OnInit {
   themes: ITheme[];
+  lastThemes: ITheme[];
 
   constructor(private themeService: ThemeService) { }
 
   ngOnInit(): void {
     this.themeService
       .getThemes()
-      .subscribe(result => { this.themes = result });
+      .subscribe(result => { this.themes = result; });
+
+    this.themeService
+      .getThemes()
+      .pipe(
+        map(events => events
+          .sort((a, b) =>
+           new Date(b.created_at).getTime() - 
+           new Date(a.created_at).getTime())))
+      .subscribe(result => { this.lastThemes = result; });
   }
 
 }
